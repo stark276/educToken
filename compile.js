e66eb41ff3ddd37f6433895cb6317b92b37378b1
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2021-07-20 13:07:15 -0700

    compile is done

--- compile.js
+++ compile.js
@@ -5,3 +5,34 @@ const solc = require('solc');
 const educTokenPath = path.resolve(__dirname, 'contract', 'EducToken.sol');
 const source = fs.readFileSync(educTokenPath, 'utf8');
 
+// console.log(source)
+
+var input = {
+  language: 'Solidity',
+  sources: {
+    'EducToken.sol': {
+      content: source
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+};
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+var contract = output.contracts['EducToken.sol']['EducToken'];
+
+var dirName = 'bin';
+
+const contractByteCodePath = path.join(dirName, 'EducToken.bin');
+
+fs.writeFileSync(contractByteCodePath, contract.evm.bytecode.object);
+
+const contractAbiPath = path.join(dirName, 'EducToken.abi');
+
+fs.writeFileSync(contractAbiPath, JSON.stringify(contract.abi));
